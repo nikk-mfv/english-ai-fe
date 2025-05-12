09a5120d3e0b1763eb93a474a480b7598f50f93e
tvnghia <nghia0969261137@gmail.com>
2025-05-12 21:58:42 +0700

    feat: input name before creating conversation

--- src/containers/home/create-conversation.tsx
+++ src/containers/home/create-conversation.tsx
@@ -1,29 +1,32 @@
-import { useCreateConversation } from '@/hooks/use-conversation';
-import { MessageCircleMore } from 'lucide-react';
-import { memo, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { toast } from 'sonner';
+import { useCreateConversation } from "@/hooks/use-conversation";
+import { MessageCircleMore } from "lucide-react";
+import { memo, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { toast } from "sonner";
 
 export const CreateConversation = memo(() => {
   const navigate = useNavigate();
   const { conversation, handleCreateConversation } = useCreateConversation();
+  const [conversationName, setConversationName] = useState<string>('')
 
-  const submit = async () => {
+  const submit = async (e: any) => {
+    e.preventDefault(); // Prevent the form from reloading the page
+    if (!conversationName || conversationName.length > 30) return
     try {
       await handleCreateConversation({
-        name: 'New Conversation',
+        name: conversationName,
         userId: 1,
       });
-      toast.success('Conversation created successfully', {
-        description: 'You can now start chatting with your new conversation',
+      toast.success("Conversation created successfully", {
+        description: "You can now start chatting with your new conversation",
         duration: 3000,
-        position: 'bottom-left',
+        position: "bottom-left",
       });
     } catch (error) {
-      toast.error('Failed to create conversation', {
+      toast.error("Failed to create conversation", {
         description: `Error: ${error}`,
         duration: 3000,
-        position: 'bottom-left',
+        position: "bottom-left",
       });
     }
   };
@@ -35,11 +38,30 @@ export const CreateConversation = memo(() => {
   }, [conversation, navigate]);
 
   return (
-    <button className='btn' onClick={submit}>
-      New Conversation
-      <MessageCircleMore />
-    </button>
+    <form onSubmit={submit} className="text-center">
+      <button className="btn mb-2" type="submit">
+        New Conversation
+        <MessageCircleMore />
+      </button>
+
+      <input
+        value={conversationName}
+        onChange={e => setConversationName(e.target.value)}
+        type="text"
+        className="input validator"
+        required
+        placeholder="Conversation name"
+        minLength={1}
+        maxLength={30}
+        title="Only letters, numbers or dash"
+      />
+      <p className="validator-hint">
+        Must be 1 to 30 characters
+        <br />
+        containing only letters, numbers or dash
+      </p>
+    </form>
   );
 });
 
-CreateConversation.displayName = 'CreateConversation';
+CreateConversation.displayName = "CreateConversation";
